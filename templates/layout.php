367958fcb957490c8d85de73ccd8e87af92cfee8
Olwen WEMPE <wempe.olwen@gmail.com>
2022-11-28 22:52:41 +0100

    added a few meta lines

--- templates/layout.php
+++ templates/layout.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title><?= $title ?></title>
-        <link href="style.css" rel="stylesheet" />
-    </head>
 
-    <body>
-        <?= $content ?>
-    </body>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link href="style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?= $content ?>
+</body>
+
 </html>
