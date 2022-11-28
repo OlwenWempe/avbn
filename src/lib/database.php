eb239563879bf950613d0d7a9f803e02de6f5719
Olwen WEMPE <wempe.olwen@gmail.com>
2022-11-28 22:39:14 +0100

    added my own logins

--- src/lib/database.php
+++ src/lib/database.php
@@ -9,7 +9,7 @@ class DatabaseConnection
     public function getConnection(): \PDO
     {
         if ($this->database === null) {
-            $this->database = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'blog', 'password');
+            $this->database = new \PDO('mysql:host=localhost;dbname=avbn_openclassroom;charset=utf8', 'olw9185en', 'Logan24092018@');
         }
 
         return $this->database;
